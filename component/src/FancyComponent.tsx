8d3d02984838d55f41f9c1f7f0bcdb10240c1be7
Molz Patrick <patrick.molz@valiton.com>
2023-01-27 14:23:43 +0100

    Clean sub component, doesn't even need a build step

--- component/src/FancyComponent.tsx
+++ component/src/FancyComponent.tsx
@@ -1,12 +1,10 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
 
 const FancyComponent: Component = () => {
   return (
-    <div class={styles.App}>
-      Hell a
+    <div>
+      Hell as
     </div>
   );
 };
